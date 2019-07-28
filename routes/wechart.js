0d81761b9f1aa4bf3c4fd70d8335707eff17e10d
yusong <1193950650@qq.com>
2019-07-28 14:32:00 +0800

    koa2

--- routes/wechart.js
+++ routes/wechart.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const sha1 = require('sha1')
 router.prefix('/wechart')
 
 router.post('/',function(ctx,next){
@@ -12,6 +13,33 @@ router.post('/',function(ctx,next){
         sendParmars = {title:'',value: ''}
     }
     ctx.body = sendParmars
+})
+
+/***********************
+*times:28/07/2019
+*author:1193950650@qq.com
+*remarks:微信token验证。
+ * 此验证为get 微信将发送一个含有微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+ * timestamp（时间戳）
+ * nonce（随机数）
+ * echostr（将要返回给微信的参数，如果加密后的数据相等）
+************************/
+router.get('/authertoken',function (ctx,next) {
+    const token = 'wechart'
+    const query = ctx.query
+
+    const signature = query.signature || ''
+    const timestamp = query.timestamp || ''
+    const nonce = query.nonce || ''
+    const echostr = query.echostr || ''
+    let str = token + timestamp + nonce
+    let sha1code = sha1(str)
+    if(sha1code == signature){
+        ctx.body = echostr
+    }else{
+        ctx.body = ''
+    }
+
 })
 
 module.exports = router
