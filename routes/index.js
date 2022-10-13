fbbb6fa029c0774e57525523fb3e4baafeecf3a8
yuszhou <yuszhou@deloitte.com.cn>
2022-10-14 01:44:52 +0800

    update service name

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuszhou
  * @Date: 2022-10-13 15:56:20
- * @LastEditTime: 2022-10-13 23:47:04
+ * @LastEditTime: 2022-10-14 01:39:48
  * @LastEditors: yuszhou
  * @Description: 
  * @FilePath: \wechartNodeTc\routes\index.js
@@ -10,7 +10,7 @@ const router = require('koa-router')()
 const wechart = require('../moudel/wecharts')
 
 router.get('/', async (ctx, next) => {
-  return await ctx.render('index',{body:JSON.stringify(new wechart('1',2))})
+  return await ctx.render('index')
 })
 
 module.exports = router
--- routes/wechart.js
+++ routes/wechart.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuszhou
  * @Date: 2022-10-13 15:56:20
- * @LastEditTime: 2022-10-14 01:20:18
+ * @LastEditTime: 2022-10-14 01:44:11
  * @LastEditors: yuszhou
  * @Description: *remarks:微信token验证。
  * 此验证为get 微信将发送一个含有微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
@@ -13,10 +13,10 @@
 const router = require('koa-router')()
 const sha1 = require('sha1')
 const wecharts = require('../moudel/wecharts')
-const wechartFn = new wecharts()
+const wechartService = new wecharts()
 router.prefix('/wechart')
 router.get('/authertoken',function (ctx,next) {
-    const token = 'wechart'
+    const token = 'wechartToken'
     const query = ctx.query
     const signature = query.signature || ''
     const timestamp = query.timestamp || ''
@@ -35,8 +35,8 @@ router.get('/authertoken',function (ctx,next) {
 //获取微信信息
 router.get('/getwechartsUserInfo',async(ctx,next)=>{
     const autherCode = ctx.request.url.split('code=')[1].split('&')[0]
-    const {access_token,openid} = await wechartFn.getwechartsOpenIdByCode(autherCode)
-    const {nickname,sex,province,city,country,headimgurl,privilege,unionid} = await wechartFn.getuserInfoByTokenAndOpenId(access_token,openid)
-
+    const {access_token,openid} = await wechartService.getwechartsOpenIdByCode(autherCode)
+    const {nickname,sex,province,city,country,headimgurl,privilege,unionid} = await wechartService.getuserInfoByTokenAndOpenId(access_token,openid)
+    const redirectUrl = await 
 })
 module.exports = router
